5b8708c0922522d5225d58a257fb2dabcb835438
Guilherme Silvestre <gaasilvestre@gmail.com>
2024-09-02 22:54:49 -0300

    Removendo personagem e adicionando shoot na barra de espaço

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,13 @@
 extends RigidBody3D
 
-var force_strength = 16.0
+@export var force_strength = 16.0
+@export var bullet_scene : PackedScene  # Referência para a cena do projétil
+@export var bullet_speed = 20.0  # Velocidade do projétil
+
+func _ready():
+	# Verifica se bullet_scene está corretamente atribuído
+	if not bullet_scene:
+		print("Bullet scene not assigned!")
 
 func _physics_process(delta):
 	var force = Vector3()
@@ -16,3 +23,19 @@ func _physics_process(delta):
 
 	if force != Vector3():
 		apply_central_force(force)
+
+	# Disparar projéteis
+	if Input.is_action_just_pressed("shoot"):  # Use is_action_just_pressed para detectar um clique
+		shoot_bullet()
+
+func shoot_bullet():
+	if bullet_scene:
+		var bullet = bullet_scene.instantiate()  # Instanciar o projétil
+		get_tree().root.add_child(bullet)  # Adicionar o projétil à cena
+		bullet.transform.origin = transform.origin  # Posicionar o projétil na posição do personagem
+		
+		# Ajustar a direção e aplicar a força ao projétil
+		var direction = transform.basis.z.normalized()  # Assumindo que o eixo z é a direção para frente
+		bullet.apply_impulse(direction * bullet_speed)
+	else:
+		print("Bullet scene is not assigned!")
